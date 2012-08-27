3e00ffb3c84aafb9f58be182a9c124085ed5ce21
Patrick Herrington <patrick.herrington.netdeveloper@gmail.com>
2012-08-26 20:53:23 -0500

    Working inginx

--- htdocs/app.js
+++ htdocs/app.js
@@ -1,14 +1,17 @@
-var mvc = require("mvc");
+var mvc = require("mvc"),
+	logger = require("winston");
+
+logger.add(logger.transports.File, { filename: 'server.log', json: false });
 
 
-mvc.server.port = 80;
+mvc.server.port = 8080;
 mvc.server.debug = true;
-mvc.server.excludes += "|app.js";
 mvc.server.defaultPath = "home";
 
 mvc.view.templateExtension = ".mu";
 mvc.view.templates.push("shared/initilizr");
 
+logger.log("info", "starting server");
 mvc.server.start();
 
 
