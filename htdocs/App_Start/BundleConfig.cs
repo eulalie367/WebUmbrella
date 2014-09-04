27627b89379abec5929557ce63b973c585ba9628
Patrick Herrington <patrick.herrington@spiral16.com>
2014-09-04 16:06:15 -0500

    Init

---
+++ htdocs/App_Start/BundleConfig.cs
@@ -0,0 +1,69 @@
+﻿using BundleTransformer.Core.Orderers;
+using BundleTransformer.Core.Transformers;
+using System.Web;
+using System.Web.Optimization;
+using BundleTransformer.Core.Builders;
+
+namespace WebUmbrella_MVC4
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.UseCdn = true;
+
+            var nullBuilder = new NullBuilder();
+            var cssTransformer = new StyleTransformer();
+            var jsTransformer = new ScriptTransformer();
+            var nullOrderer = new NullOrderer();
+
+            var jquery = new Bundle("~/bundles/jquery", "//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js");
+            jquery.Include("~/js/vendor/jquery-1.11.0.min.js");
+            jquery.Builder = nullBuilder;
+            jquery.Transforms.Add(jsTransformer);
+            jquery.Orderer = nullOrderer;
+            jquery.CdnFallbackExpression = "window.jquery";
+
+            bundles.Add(jquery);
+
+
+            var main = new ScriptBundle("~/bundles/main");
+            main.Include
+            (
+                    "~/js/plugins.js",
+                    "~/js/main.js"
+            );
+            main.Builder = nullBuilder;
+            main.Transforms.Add(jsTransformer);
+            main.Orderer = nullOrderer;
+
+            bundles.Add(main);
+
+
+            var modernizr = new ScriptBundle("~/bundles/modernizr");
+            modernizr.Include
+            (
+                    "~/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"
+            );
+            modernizr.Builder = nullBuilder;
+            modernizr.Transforms.Add(jsTransformer);
+            modernizr.Orderer = nullOrderer;
+
+            bundles.Add(modernizr);
+
+
+            var less = new Bundle("~/bundles/less");
+            less.Include
+            (
+                "~/less/normalize.less",
+                "~/less/main.less"
+            );
+            less.Builder = nullBuilder;
+            less.Transforms.Add(cssTransformer);
+            less.Orderer = nullOrderer;
+
+            bundles.Add(less);
+        }
+    }
+}
---
+++ htdocs/Controllers/ControllerBase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace WebUmbrella_MVC4
+{
+    public class ControllerBase : Controller
+    {
+
+        public ControllerBase()
+        {
+            this.ViewEngineCollection.Clear();
+            this.ViewEngineCollection.Add(new CustomViewEngine());
+        }
+
+        public string RenderRazorViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (var sw = new System.IO.StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+
+        public class CustomViewEngine : RazorViewEngine
+        {
+            public CustomViewEngine()
+            {
+                var viewLocations = new[] 
+                {  
+                    "~/Views/{0}.cshtml",  
+                    "~/Views/{1}/{0}.cshtml"
+                };
+                SetViewEngine(viewLocations);
+            }
+            public CustomViewEngine(string[] viewLocations)
+            {
+                SetViewEngine(viewLocations);
+            }
+            public static void SetViewEngine(string viewLocation, ViewEngineCollection viewEngineCollection)
+            {
+                viewEngineCollection.Clear();
+                CustomViewEngine cve = new CustomViewEngine();
+                cve.SetViewEngine(viewLocation);
+                viewEngineCollection.Add(cve);
+            }
+            public static void SetViewEngine(string[] viewLocation, ViewEngineCollection viewEngineCollection)
+            {
+                viewEngineCollection.Clear();
+                CustomViewEngine cve = new CustomViewEngine();
+                cve.SetViewEngine(viewLocation);
+                viewEngineCollection.Add(cve);
+            }
+            public void SetViewEngine(string viewLocation)
+            {
+                SetViewEngine(new string[] { viewLocation });
+            }
+            public void SetViewEngine(string[] viewLocations)
+            {
+                this.PartialViewLocationFormats = viewLocations;
+                this.ViewLocationFormats = viewLocations;
+            }
+        }
+        public class HttpParamActionAttribute : ActionNameSelectorAttribute
+        {
+            public override bool IsValidName(ControllerContext controllerContext, string actionName, System.Reflection.MethodInfo methodInfo)
+            {
+                if (actionName.Equals(methodInfo.Name, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+
+                var request = controllerContext.RequestContext.HttpContext.Request;
+                return request[methodInfo.Name] != null;
+            }
+        }
+    }
+}
